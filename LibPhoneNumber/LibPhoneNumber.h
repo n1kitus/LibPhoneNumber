82d6e2ed18b2b6d3763edb9a73423504e70f06e3
Nikita Dobrov <nikitadobrov@gmail.com>
2019-04-03 14:07:00 +0300

    Initial Commit

---
+++ LibPhoneNumber/LibPhoneNumber.h
@@ -0,0 +1,19 @@
+//
+//  LibPhoneNumber.h
+//  LibPhoneNumber
+//
+//  Created by Nikita Dobrov on 03/04/2019.
+//  Copyright © 2019 Nikita Dobrov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LibPhoneNumber.
+FOUNDATION_EXPORT double LibPhoneNumberVersionNumber;
+
+//! Project version string for LibPhoneNumber.
+FOUNDATION_EXPORT const unsigned char LibPhoneNumberVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LibPhoneNumber/PublicHeader.h>
+
+
